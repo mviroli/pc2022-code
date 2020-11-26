7155423413da59ecd896e9d48fd6bcfe453d2f59
mirko.viroli <mirko.viroli@unibo.it>
2020-11-26 17:08:43 +0100

    lesson lab1 ready

--- src/main/scala/lab/demo/Programs.scala
+++ src/main/scala/lab/demo/Programs.scala
@@ -1,25 +1,43 @@
 package lab.demo
 
 import it.unibo.scafi.incarnations.BasicAbstractIncarnation
+import it.unibo.scafi.simulation.s2.frontend.incarnation.scafi.bridge.ExportEvaluation.EXPORT_EVALUATION
 import it.unibo.scafi.simulation.s2.frontend.incarnation.scafi.bridge.SimulationInfo
-import it.unibo.scafi.simulation.s2.frontend.incarnation.scafi.configuration.ScafiProgramBuilder
+import it.unibo.scafi.simulation.s2.frontend.incarnation.scafi.configuration.{ScafiProgramBuilder, ScafiWorldInformation}
 import it.unibo.scafi.simulation.s2.frontend.incarnation.scafi.world.ScafiWorldInitializer.Random
+import it.unibo.scafi.simulation.s2.frontend.incarnation.scafi.bridge.ScafiWorldIncarnation.EXPORT
 import it.unibo.scafi.simulation.s2.frontend.view.{ViewSetting, WindowConfiguration}
 import lab.gui.patch.RadiusLikeSimulation
+import it.unibo.scafi.space.graphics2D.BasicShape2D.Circle
 
 object Incarnation extends BasicAbstractIncarnation
 import lab.demo.Incarnation._ //import all stuff from an incarnation
 
 object Simulation extends App {
+
+  val formatter_evaluation: EXPORT_EVALUATION[Any] = (e : EXPORT) => formatter(e.root[Any]())
+
+  val formatter: Any => Any = (e) => e match {
+    case (a,b) => (formatter(a),formatter(b))
+    case (a,b,c) => (formatter(a),formatter(b),formatter(c))
+    case (a,b,c,d) => (formatter(a),formatter(b),formatter(c),formatter(d))
+    case l:Iterable[_] => l.map(formatter(_)).toString
+    case i: java.lang.Number if (i.doubleValue()>10000) => "Inf"
+    case i: java.lang.Number if (-i.doubleValue()>10000) => "-Inf"
+    case i: java.lang.Double => f"${i.doubleValue()}%1.2f"
+    case x => x.toString
+  }
+
   val programClass = classOf[Main16]
-  val nodes = 50
+  val nodes = 100
   val neighbourRange = 200
-  val (width, height) = (1024, 768)
+  val (width, height) = (1920, 1080)
   ViewSetting.windowConfiguration = WindowConfiguration(width, height)
   ScafiProgramBuilder (
     Random(nodes, width, height),
-    SimulationInfo(programClass),
+    SimulationInfo(programClass,exportEvaluations = List(formatter_evaluation)),
     RadiusLikeSimulation(neighbourRange),
+    ScafiWorldInformation(shape = Some(Circle(5,5))),
     neighbourRender = true,
   ).launch()
 }
